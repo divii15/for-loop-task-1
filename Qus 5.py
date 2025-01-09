bd43d188df555f4a5ba10974c8b2f8ddde76bae8
divii15 <divyadaksha143@gmail.com>
2025-01-09 10:47:40 +0530

    My new task 09/01

--- Qus 5.py
+++ Qus 5.py
@@ -1,6 +1,5 @@
 # 5) print your name reverse
 
 
-name=input("Enter the name:")  
-for i in range(len(name)-1,-1,-1):
-    print(name[i],end="")
+name=input("Enter the name:") 
+print("The reversed name is ",name[::-1])
